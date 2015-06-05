68fc6ca9153fd5b6609b49bb60bd2b233d82851a
Chris Powell <powellchristoph@gmail.com>
2015-06-05 16:18:30 -0400

    Add build wheel.

--- setup.py
+++ setup.py
@@ -7,20 +7,18 @@ REQUIRES = ['pyrax', 'paramiko', 'python-gnupg']
             
 setup(
     name='rapture',
-    version='0.1',
+    version='0.1.0',
     description='Rapture - watches a directory and uploads files to the cloud',
     license='Apache License 2.0',
     url='github.com/powellchristoph/rapture',
     author='Chris Powell',
     author_email='powellchristoph@gmail.com',
     install_requires=REQUIRES,
-    test_suite='rapture',
-    zip_safe=False,
+    #test_suite='rapture',
+    #zip_safe=False,
     entry_points={
         'console_scripts': [
             'rapture-app= rapture.app:run',
         ]
     },
-#    data_files=[('config', ['etc/rapture.conf', 'ini/configspec.ini'])],
-    packages=find_packages(exclude=['tests*', 'deuce/tests*'])
 )
